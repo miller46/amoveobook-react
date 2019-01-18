09b7893eaca43b0b2148e7799c2c5e9d4485a687
miller46 <jack@Jacks-MacBook-Pro.local>
2019-01-18 15:01:18 -0600

    fix price decimals

--- src/config/index.js
+++ src/config/index.js
@@ -4,6 +4,6 @@ export {api} from './api'
 export {images} from './images'
 
 export const tokenDecimals = 100000000;
-export const priceDecimals = 100000;
+export const priceDecimals = 10000;
 export const tokenUnit = "VEO";
 
