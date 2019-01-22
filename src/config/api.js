325b5d017b635e3c6ca095f070353a00fa64c6ee
miller46 <jack@Jacks-MacBook-Pro.local>
2019-01-22 01:12:39 -0600

    release version

--- src/config/api.js
+++ src/config/api.js
@@ -1,6 +1,6 @@
 import {MAINNET, TESTNET} from "../constants";
 
-const isTestnet = true;
+const isTestnet = false;
 const isDev = false;
 
 const apiBase = isDev ? "http://127.0.0.1:5001/api/v1/": "https://amoveobook-api.herokuapp.com/api/v1/"
@@ -20,7 +20,6 @@ export const api = {
 	getIpUrl: "https://api.ipify.org/?format=json",
 	getIpDetailsUrl: "http://api.ipstack.com/$$$?access_key=4db6b276661ad034f44477078402a553&format=1",
 	replacementCharacter: "$$$",
-	// defaultNodeUrl: isTestnet ? "http://127.0.0.1:8070/" : "http://40.117.196.55:8080/",
-	defaultNodeUrl: isTestnet ? "http://127.0.0.1:5001/api/v1/node/" : "http://40.117.196.55:8080/",
+	defaultNodeUrl: apiBase + "node",
 	serverPublicKey: isTestnet ? "BKw7qHfymuCuUnakeUJW/qhrz4MBzwBrIxJq8hihW5Do6zVQtA8C9t5DBHTrWZ5GT5SKnRztKz+JvpC2BYpsn2Y=" : "BKSYQw/88NOUfwBhyXwVuKymkQxZNxJ0NjqAqJQgXKvflmRJ1K1Q70WAIsUtmxa6C30FdcQKPzeEarNIT4mCUKU=",
 }
