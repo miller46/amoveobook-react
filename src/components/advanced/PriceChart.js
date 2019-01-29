185cb81f15c4569c420bfd4a520c04c09d2fd532
miller46 <jack@Jacks-MacBook-Pro.local>
2019-01-28 19:24:38 -0600

    add price chart buttons

--- src/components/advanced/PriceChart.js
+++ src/components/advanced/PriceChart.js
@@ -12,8 +12,8 @@ export default class PriceChart extends Component {
 		super(props);
 		this.state = {
 			prices: this.props.prices,
-			selectedLength: 1,
-			selectedInterval: 60,
+			selectedLength: "all",
+			selectedInterval: "1h",
 		}
 	}
 
@@ -106,88 +106,234 @@ export default class PriceChart extends Component {
 		}
 	}
 
+	setInterval(interval) {
+		const {selectedInterval, selectedLength} = this.state;
+		if (interval !== selectedInterval) {
+			// this is nutty but basically if the interval is less than the total chart length
+			// then respect the user's new interval choice and set chart length to all
+			if (interval === "1d" && (selectedLength === "1h" || selectedLength === "1d")
+				|| (interval === "6h" && selectedLength === "1h")
+				|| (interval === "1h" && selectedLength === "1h") ) {
+				this.setState({selectedInterval: interval, selectedLength: "all"})
+			} else {
+				this.setState({selectedInterval: interval})
+			}
+		}
+	}
+
+	setLength(len) {
+		const {selectedInterval, selectedLength} = this.state;
+		if (len !== selectedLength) {
+			if (len === "1h" && (selectedInterval === "1h" || selectedInterval === "1d")) {
+				this.setState({selectedLength: len, selectedInterval: "5m"})
+			} else if (len === "1d" && selectedInterval === "1d") {
+				this.setState({selectedLength: len, selectedInterval: "1h"})
+			} else {
+				this.setState({selectedLength: len})
+			}
+		}
+	}
+
 	render() {
-		let {prices, selectedLength, selectedInterval} = this.state;
+		const {prices, selectedLength, selectedInterval} = this.state;
 
 		let ohlc = [];
 		let volume = [];
 
-		const utcOffset = 6 * 60 * 60 * 1000;
-		const end = Date.parse(new Date());
-		const begin = end - selectedLength * 24 * 60 * 60 * 1000
-		const interval = selectedInterval * 60 * 1000;
+		if (prices.length > 0) {
+			const utcOffset = 6 * 60 * 60 * 1000;
+			const end = Date.parse(new Date());
 
-		let lastOpen = -1;
-		let lastHigh = 0;
-		let lastLow = 9999999999;
-		let lastClose = 0;
+			let begin;
+			if (selectedLength === "1h") {
+				begin = end - (1 * 60 * 60 * 1000)
+			} else if (selectedLength === "1d") {
+				begin = end - (24 * 60 * 60 * 1000)
+			} else if (selectedLength === "1m") {
+				begin = end - (30 * 24 * 60 * 60 * 1000)
+			} else {
+				// fallback beginning of bets
+				const firstPrice = prices[0];
+				begin = Date.parse(firstPrice.timestamp) - utcOffset - 1
+			}
 
-		for (let i = begin; i < end; i = i + interval) {
-			const date = i;
+			// back stop for first order anyway
+			const firstPrice = prices[0];
+			const firstOrderDate = Date.parse(firstPrice.timestamp) - utcOffset - 1
+			if (begin < firstOrderDate) {
+				begin = firstOrderDate
+			}
 
-			let totalAmount = 0;
+			let interval;
+			if (selectedInterval === "1m") {
+				interval = 1 * 60 * 1000;
+			} else if (selectedInterval === "5m") {
+				interval = 5 * 60 * 1000;
+			} else if (selectedInterval === "15m") {
+				interval = 15 * 60 * 1000;
+			} else if (selectedInterval === "1h") {
+				interval = 60 * 60 * 1000;
+			} else if (selectedInterval === "6h") {
+				interval = 6 * 60 * 60 * 1000;
+			} else if (selectedInterval === "1d") {
+				interval = 24 * 60 * 60 * 1000;
+			} else {
+				// fallback 1 hour
+				interval = 60 * 60 * 1000;
+			}
 
-			for (let lastIndex = 0; lastIndex < prices.length; lastIndex++) {
-				const order = prices[lastIndex];
-				const orderDate = Date.parse(order.timestamp) - utcOffset
+			let lastOpen = -1;
+			let lastHigh = 0;
+			let lastLow = 9999999999;
+			let lastClose = 0;
 
-				if (orderDate >= date && date + interval > orderDate) {
+			for (let i = begin; i < end; i = i + interval) {
+				const date = i;
 
-					const price = 100 * order.price / priceDecimals;
-					if (price > lastHigh) {
-						lastHigh = price;
+				let totalAmount = 0;
+
+				for (let lastIndex = 0; lastIndex < prices.length; lastIndex++) {
+					const order = prices[lastIndex];
+					const orderDate = Date.parse(order.timestamp) - utcOffset
+
+					if (orderDate >= date && date + interval > orderDate) {
+
+						const price = 100 * order.price / priceDecimals;
+						if (price > lastHigh) {
+							lastHigh = price;
+						}
+
+						if (price < lastLow) {
+							lastLow = price;
+						}
+
+						if (lastOpen === -1) {
+							lastOpen = price
+						}
+
+						lastClose = price;
+
+						const amount = order.buy_amount / tokenDecimals;
+						totalAmount = totalAmount + amount;
 					}
-
-					if (price < lastLow) {
-						lastLow = price;
-					}
-
-					if (lastOpen === -1) {
-						lastOpen = price
-					}
-
-					lastClose = price;
-
-					const amount = order.buy_amount / tokenDecimals;
-					totalAmount = totalAmount + amount;
 				}
+
+				if (lastLow === 9999999999) {
+					lastLow = 0;
+				}
+
+				if (lastOpen < 0) {
+					lastOpen = 0;
+				}
+
+				ohlc.push([
+					date, // the date
+					lastOpen, // open
+					lastHigh, // high
+					lastLow, // low
+					lastClose // close
+				]);
+
+				volume.push([
+					date, // the date
+					totalAmount // the volume
+				]);
+
+				lastOpen = lastClose;
+				lastHigh = lastClose;
+				lastLow = lastClose;
 			}
-
-			if (lastLow === 9999999999) {
-				lastLow = 0;
-			}
-
-			if (lastOpen < 0) {
-				lastOpen = 0;
-			}
-
-			ohlc.push([
-				date, // the date
-				lastOpen, // open
-				lastHigh, // high
-				lastLow, // low
-				lastClose // close
-			]);
-
-			volume.push([
-				date, // the date
-				totalAmount // the volume
-			]);
-
-			lastOpen = lastClose;
-			lastHigh = lastClose;
-			lastLow = lastClose;
 		}
 
 		const options = this.getOptions(ohlc, volume);
 
 		return (
-			<div styleName="ChartContainer">
-				<HighchartsReact
-					highcharts={Highcharts}
-					constructorType={'stockChart'}
-					options={options}
-				/>
+			<div>
+				<table styleName="Buttons">
+					<tbody>
+					<tr>
+						<td>
+							<div styleName="ChartLength">
+								<p
+									styleName={selectedLength === "1h" ? "ButtonSelected" : ""}
+									onClick={() => this.setLength("1h")}>
+									1h
+								</p>
+
+								<p
+									styleName={selectedLength === "1d" ? "ButtonSelected" : ""}
+									onClick={() => this.setLength("1d")}>
+									1d
+								</p>
+								<p
+									styleName={selectedLength === "1w" ? "ButtonSelected" : ""}
+									onClick={() => this.setLength("1w")}>
+									1w
+								</p>
+								<p
+									styleName={selectedLength === "1m" ? "ButtonSelected" : ""}
+									onClick={() => this.setLength("1m")}>
+									1m
+								</p>
+								<p
+									styleName={selectedLength === "all" ? "ButtonSelected" : ""}
+									onClick={() => this.setLength("all")}>
+									all
+								</p>
+							</div>
+						</td>
+
+						<td>
+							<div styleName="PanelTitle">
+								<p>Price Chart</p>
+							</div>
+						</td>
+
+						<td>
+							<div styleName="ChartInterval">
+								<p
+									styleName={selectedInterval === "1m" ? "ButtonSelected" : ""}
+									onClick={() => this.setInterval("1m")}>
+									1m
+								</p>
+								<p
+									styleName={selectedInterval === "5m" ? "ButtonSelected" : ""}
+									onClick={() => this.setInterval("5m")}>
+									5m
+								</p>
+								<p
+									styleName={selectedInterval === "15m" ? "ButtonSelected" : ""}
+									onClick={() => this.setInterval("15m")}>
+									15m
+								</p>
+								<p
+									styleName={selectedInterval === "1h" ? "ButtonSelected" : ""}
+									onClick={() => this.setInterval("1h")}>
+									1h
+								</p>
+								<p
+									styleName={selectedInterval === "6h" ? "ButtonSelected" : ""}
+									onClick={() => this.setInterval("6h")}>
+									6h
+								</p>
+								<p
+									styleName={selectedInterval === "1d" ? "ButtonSelected" : ""}
+									onClick={() => this.setInterval("1d")}>
+									1d
+								</p>
+							</div>
+						</td>
+					</tr>
+					</tbody>
+				</table>
+
+				<div styleName="ChartContainer">
+					<HighchartsReact
+						highcharts={Highcharts}
+						constructorType={'stockChart'}
+						options={options}
+					/>
+				</div>
 			</div>
 		)
 	}
