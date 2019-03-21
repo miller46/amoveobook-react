cc7cd174ee5b5d71eeecc6e3174bb50791e854e2
johnnycash77 <jack@Jacks-MacBook-Pro.local>
2019-03-21 17:29:48 -0500

    add faq item for channel state

--- src/containers/FAQ.js
+++ src/containers/FAQ.js
@@ -35,6 +35,25 @@ export default class FAQ extends Component {
 						You can close the channel at any time, at which point the tab gets settled and any remaining money gets returned to you.
 					</p>
 				</div>
+
+				<div styleName="Question" name="channel-out-of-sync">
+					<p styleName="Title">"Your channel is out of sync." What does that mean?</p>
+
+					<p styleName="Body">
+						This means your record of orders stored in your Amoveo3 wallet is not the same as Amoveobook's.
+						This can happen if you are using Amoveobook across multiple devices.  The Amoveo3 wallet does not store any data in any server.
+						This channel state is the information sent to the blockchain once a channel is closed so that the correct amount of VEO is returned to you.
+					</p>
+
+					<p styleName="Body">
+						&nbsp;
+					</p>
+
+					<p styleName="Body">
+						Note that by syncing with the Amoveobook channel state your local copy will be overwritten.
+						If you have a channel, you can backup your old channel state in the Amoveo3 wallet by clicking the 'Export' button in the 'Channels' tab.
+					</p>
+				</div>
 			</div>
 		)
 	}
